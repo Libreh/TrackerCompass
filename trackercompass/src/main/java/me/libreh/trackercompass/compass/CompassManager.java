461aa8bf923fcaeaa1707f2d5a72827ced762a79
libreh <beverlydlashley@protonmail.com>
2026-04-19 16:25:38 +0200

    Extract ActionBarRenderer and CompassHelper API

--- trackercompass/src/main/java/me/libreh/trackercompass/compass/CompassManager.java
+++ trackercompass/src/main/java/me/libreh/trackercompass/compass/CompassManager.java
@@ -1,15 +1,10 @@
 package me.libreh.trackercompass.compass;
 
+import me.libreh.trackercompass.api.CompassHelper;
 import me.libreh.trackercompass.config.ConfigManager;
 import me.libreh.trackercompass.data.TrackerCompassSavedData;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.GlobalPos;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.LodestoneTracker;
-
-import java.util.Optional;
 
 public class CompassManager {
     private final TrackerCompassSavedData data;
@@ -20,59 +15,20 @@ public class CompassManager {
 
     public void update(ServerPlayer player, BlockPos targetPos) {
         if (targetPos != null) {
-            pointToTarget(player, targetPos);
+            CompassHelper.pointToTarget(player, targetPos, TrackerCompassItem::isTrackerCompass);
         }
     }
 
     public void syncInventory(ServerPlayer player) {
         if (hasCompassEnabled(player)) {
-            giveCompass(player);
+            CompassHelper.giveCompass(player, TrackerCompassItem::create, TrackerCompassItem::isTrackerCompass);
         } else {
-            removeCompasses(player);
+            CompassHelper.removeCompasses(player, TrackerCompassItem::isTrackerCompass);
         }
     }
 
     private boolean hasCompassEnabled(ServerPlayer player) {
         Boolean toggle = data.getPlayerCompassToggle(player.getUUID());
         return toggle != null ? toggle : ConfigManager.config().giveCompassByDefault;
-    }
-
-    private void giveCompass(ServerPlayer player) {
-        int found = -1;
-        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            ItemStack stack = player.getInventory().getItem(i);
-            if (TrackerCompassItem.isTrackerCompass(stack)) {
-                if (found == -1) {
-                    found = i;
-                } else {
-                    player.getInventory().removeItemNoUpdate(i);
-                }
-            }
-        }
-
-        if (found == -1) {
-            player.getInventory().add(TrackerCompassItem.create());
-        }
-    }
-
-    private void removeCompasses(ServerPlayer player) {
-        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            ItemStack stack = player.getInventory().getItem(i);
-            if (TrackerCompassItem.isTrackerCompass(stack)) {
-                player.getInventory().removeItemNoUpdate(i);
-            }
-        }
-    }
-
-    private void pointToTarget(ServerPlayer player, BlockPos targetPos) {
-        GlobalPos globalPos = GlobalPos.of(player.level().dimension(), targetPos);
-        LodestoneTracker tracker = new LodestoneTracker(Optional.of(globalPos), false);
-
-        for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-            ItemStack stack = player.getInventory().getItem(i);
-            if (TrackerCompassItem.isTrackerCompass(stack)) {
-                stack.set(DataComponents.LODESTONE_TRACKER, tracker);
-            }
-        }
     }
 }
