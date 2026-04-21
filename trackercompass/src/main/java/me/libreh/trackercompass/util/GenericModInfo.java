28294b1cb65e9951b2384596ed0959d396ee75af
libreh <beverlydlashley@protonmail.com>
2026-04-21 17:59:57 +0200

    Update GenericModInfo

--- trackercompass/src/main/java/me/libreh/trackercompass/util/GenericModInfo.java
+++ trackercompass/src/main/java/me/libreh/trackercompass/util/GenericModInfo.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 
 import javax.imageio.ImageIO;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.util.ArrayList;
@@ -18,41 +19,61 @@ public class GenericModInfo {
     private static final int VERSION_LABEL_COLOR = 0xF7E1A7;
     private static final int GITHUB_LINK_COLOR = 0x58A6FF;
 
-    private static Component[] icon = new Component[0];
-    private static Component[] about = new Component[0];
-    private static Component[] consoleAbout = new Component[0];
+    private static ModContainer container;
+    private static String id;
+    private static Logger logger;
+    private static boolean showModrinth;
+    private static boolean showGitHub;
+    private static int titleColor;
+
+    private static Component[] icon;
+    private static Component[] about;
+    private static Component[] consoleAbout;
 
     public static void build(ModContainer container, String id, Logger logger, boolean showModrinth, boolean showGitHub, int titleColor) {
-        buildIcon(container, id, logger);
-        buildAbout(container, id, logger, showModrinth, showGitHub, titleColor);
+        GenericModInfo.container = container;
+        GenericModInfo.id = id;
+        GenericModInfo.logger = logger;
+        GenericModInfo.showModrinth = showModrinth;
+        GenericModInfo.showGitHub = showGitHub;
+        GenericModInfo.titleColor = titleColor;
     }
 
-    private static void buildIcon(ModContainer container, String id, Logger logger) {
+    private static void ensureBuilt() {
+        if (about != null) return;
+        buildIcon();
+        buildAbout();
+    }
+
+    private static void buildIcon() {
         var iconLines = new ArrayList<MutableComponent>();
+        var filePath = container.findPath("assets/" + id + "/icon.png");
+
         try {
-            var filePath = container.findPath("assets/" + id + "/icon.png");
             if (filePath.isEmpty()) throw new FileNotFoundException("Icon not found");
 
-            var source = ImageIO.read(Files.newInputStream(filePath.get()));
-            for (int y = 0; y < source.getHeight(); y++) {
-                var line = Component.literal("");
-                int runLength = 0, currentColor = source.getRGB(0, y) & 0xFFFFFF;
+            try (InputStream stream = Files.newInputStream(filePath.get())) {
+                var source = ImageIO.read(stream);
+                for (int y = 0; y < source.getHeight(); y++) {
+                    var line = Component.literal("");
+                    int runLength = 0, currentColor = source.getRGB(0, y) & 0xFFFFFF;
 
-                for (int x = 0; x < source.getWidth(); x++) {
-                    int pixelColor = source.getRGB(x, y) & 0xFFFFFF;
-                    if (currentColor == pixelColor) {
-                        runLength++;
-                    } else {
-                        line.append(Component.literal("█".repeat(runLength))
-                                .setStyle(Style.EMPTY.withColor(currentColor).withShadowColor(currentColor | 0xFF000000)));
-                        currentColor = pixelColor;
-                        runLength = 1;
+                    for (int x = 0; x < source.getWidth(); x++) {
+                        int pixelColor = source.getRGB(x, y) & 0xFFFFFF;
+                        if (currentColor == pixelColor) {
+                            runLength++;
+                        } else {
+                            line.append(Component.literal("█".repeat(runLength))
+                                    .setStyle(Style.EMPTY.withColor(currentColor).withShadowColor(currentColor | 0xFF000000)));
+                            currentColor = pixelColor;
+                            runLength = 1;
+                        }
                     }
-                }
 
-                line.append(Component.literal("█".repeat(runLength))
-                        .setStyle(Style.EMPTY.withColor(currentColor).withShadowColor(currentColor | 0xFF000000)));
-                iconLines.add(line);
+                    line.append(Component.literal("█".repeat(runLength))
+                            .setStyle(Style.EMPTY.withColor(currentColor).withShadowColor(currentColor | 0xFF000000)));
+                    iconLines.add(line);
+                }
             }
         } catch (Throwable e) {
             logger.warn("Error building icon", e);
@@ -65,7 +86,7 @@ public class GenericModInfo {
         icon = iconLines.toArray(new Component[0]);
     }
 
-    private static void buildAbout(ModContainer container, String id, Logger logger, boolean showModrinth, boolean showGitHub, int titleColor) {
+    private static void buildAbout() {
         var fullAbout = new ArrayList<Component>();
         var basicAbout = new ArrayList<Component>();
 
@@ -141,12 +162,12 @@ public class GenericModInfo {
 
             var output = new ArrayList<Component>();
             if (icon.length > fullAbout.size() + 2) {
-                int ComponentIndex = 0, startLine = (icon.length - fullAbout.size() - 1) / 2;
+                int componentIndex = 0, startLine = (icon.length - fullAbout.size() - 1) / 2;
                 for (int i = 0; i < icon.length; i++) {
-                    if (i >= startLine && ComponentIndex < fullAbout.size()) {
+                    if (i >= startLine && componentIndex < fullAbout.size()) {
                         output.add(icon[i].copy()
                                 .append(Component.literal("  ").setStyle(Style.EMPTY.withItalic(false)))
-                                .append(fullAbout.get(ComponentIndex++)));
+                                .append(fullAbout.get(componentIndex++)));
                     } else {
                         output.add(icon[i]);
                     }
@@ -169,10 +190,12 @@ public class GenericModInfo {
     }
 
     public static Component[] getAboutFull() {
+        ensureBuilt();
         return about;
     }
 
     public static Component[] getAboutConsole() {
+        ensureBuilt();
         return consoleAbout;
     }
 }
